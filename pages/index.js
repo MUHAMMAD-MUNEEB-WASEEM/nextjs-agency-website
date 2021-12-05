7adb8f6858eb0ff4d4d6fd236f82a6967bbb859e
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-12-05 08:58:09 +0500

    Created and designed service component

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,14 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 import Intro from '../components/Intro'
 import Navbar from '../components/Navbar'
+import Services from '../components/Services'
+import { data } from '../data'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({services}) {
+
+  //console.log(services)
+
   return (
     <div>
       <Head>
@@ -13,6 +18,30 @@ export default function Home() {
         <meta name="description" content="Made using Nextjs" />
       </Head>
       <Intro/>
+      <Services services={services}/>
     </div>
   )
+}
+
+//Fetching data before rendering above components
+//Below part should be done on that page where we 
+//want to show that data, here we show cards on services page
+
+
+export const getStaticProps = async () => {
+
+  // if api
+  //  const services =  await axios.get('/', req,res,)=>{
+
+  // }
+
+  //Static data from file
+  const services = data
+
+  return {
+    props : { services }
+  }
+
+//Now we use these props immediately in this page as shown in home function above
+//Pass these services props in services components and thus we can use this in services component
 }
